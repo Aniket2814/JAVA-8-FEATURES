78f64303d49b0972bf528c1d209debe08e8c4038
Aniket Rathore <aniketrathore911@gmail.com>
2025-03-13 18:09:36 +0530

    Let's Start Optional class

---
+++ Optional1.java
@@ -0,0 +1,14 @@
+class Optional1
+{
+public static void main(String ar[])
+{
+String name=getName(1);
+System.out.println(name);
+
+}
+public static String getName(int id)
+{
+	// getting Name from db
+return "Ram";
+}
+}
